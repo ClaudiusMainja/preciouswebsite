734bc279a1c7625dbc04fcf2b12c63363eb9057c
Claudius Mainja <cmainja@gmail.com>
2024-12-10 15:49:12 +0200

    .

---
+++ js/script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                'heading': ['lovers-quarrel-regular', ''],
+                'body': ['Open Sans', 'sans-serif']
+            },
+            colors: {
+                'brand-pink': {
+                    50: '#fdf2f8',
+                    100: '#fce7f3',
+                    500: '#ec4899',
+                    600: '#db2777',
+                    700: '#be185d'
+                }
+            }
+        }
+    }
+}
